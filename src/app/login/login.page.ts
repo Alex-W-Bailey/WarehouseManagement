d11564261dc6252fcdd01df8016d64c6a3f3d858
xKingAlex <alexwbailey7@gmail.com>
2020-05-07 16:10:24 -0500

    remeber login UI

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -11,6 +11,8 @@ export class LoginPage implements OnInit {
   username: string = "";
   password: string = "";
 
+  toggledRememberLogin: boolean = false;
+
   constructor(private api: ApiService) { }
 
   ngOnInit() {
@@ -21,23 +23,14 @@ export class LoginPage implements OnInit {
 
     console.log("username: ", username + " password: ", password);
 
-    var usernameIonItem = document.getElementById("ion-username");
-    var passwordIonItem = document.getElementById("ion-password");
-
     if(username === "" || username == undefined) {
-      console.log("username blank... val: " + username);
-
-      usernameIonItem.classList.add("input-err");
+      this.showError("username", "Username Is Required!");      
     }
     else if(password === "" || password == undefined) {
-      console.log("password blank... val: " + password);
-      
-      usernameIonItem.classList.remove("input-err");
-      passwordIonItem.classList.add("input-err");
+      this.showError("password", "Password Is Required!");      
     }
     else {
-      usernameIonItem.classList.remove("input-err");
-      passwordIonItem.classList.remove("input-err");
+      this.hideError();
 
       this.api.attemptLogin(username, password).subscribe((result) => {
         var userInfo = result[0];
@@ -50,4 +43,53 @@ export class LoginPage implements OnInit {
       });
     }
   } 
+
+  showError(elementErr, errMsg) {
+    var usernameIonItem = document.getElementById("ion-username");
+    var passwordIonItem = document.getElementById("ion-password");
+
+    var errorSection = document.getElementById("error-section");
+    var errorText = document.getElementById("error-text");
+
+    errorSection.classList.remove("hide");
+    errorText.innerHTML = errMsg;
+    
+    if(elementErr == "username") {
+      usernameIonItem.classList.add("input-err");
+      passwordIonItem.classList.remove("input-err");
+    }
+    else {
+      usernameIonItem.classList.remove("input-err");
+      passwordIonItem.classList.add("input-err");
+    }
+  }
+
+  hideError() {
+    var usernameIonItem = document.getElementById("ion-username");
+    var passwordIonItem = document.getElementById("ion-password");
+
+    var errorSection = document.getElementById("error-section");
+    var errorText = document.getElementById("error-text");
+
+    usernameIonItem.classList.remove("input-err");
+    passwordIonItem.classList.remove("input-err");
+    errorSection.classList.add("hide");
+  }
+
+  toggleRemember() {
+    var btn = document.getElementById("toggleRememberBtn");
+
+    if(this.toggledRememberLogin === false) {
+      this.toggledRememberLogin = true;
+      btn.classList.remove("white-text");
+      btn.classList.add("green-text");
+    }
+    else {
+      this.toggledRememberLogin = false
+      btn.classList.remove("green-text");
+      btn.classList.add("white-text");
+    }
+
+    console.log("toggled set to: " + this.toggledRememberLogin);
+  }
 }
